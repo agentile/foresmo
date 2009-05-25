9dadcdddc5be9a42372b1b04beaba5fecfaf5415
Anthony <asgentile@gmail.com>
2009-05-25 17:48:58 -0400

    adding some initial installation

---
+++ source/foresmo/Foresmo/App/Admin.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Foresmo_App_Admin
+ * Admin Controller
+ *
+ * @category  App
+ * @package   Foresmo
+ * @author    Anthony Gentile, Bryden Tweedy
+ * @version   0.09
+ * @since     0.05
+ */
+class Foresmo_App_Admin extends Foresmo_App_Base {
+
+    protected $_layout_default = 'admin';
+    protected $_action_default = 'index';
+
+    /**
+     * actionIndex
+     * Default install action/page
+     *
+     * @return void
+     *
+     * @access public
+     * @since .09
+     */
+    public function actionIndex()
+    {
+        if ($this->session->get('Foresmo_username', false) === false
+            || !$this->session->get('Foresmo_username')) {
+            $this->_redirect('/login');
+        }
+    }
+}
---
+++ source/foresmo/Foresmo/App/Base/Layout/admin.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Foresmo - Dev</title>
+<?php
+echo $this->style('Foresmo/styles/install.css')."\n";
+echo $this->script('Foresmo/scripts/mootools-1.2.2-core.js')."\n";
+echo $this->script('Foresmo/scripts/mootools-1.2.2.2-more.js')."\n";
+?>
+</head>
+<body>
+
+<?php echo $this->layout_content; ?>
+
+</body>
+</html>
