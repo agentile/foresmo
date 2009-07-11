4dc0b130f949d0e8f94fea26e5cf33efdde0f116
Anthony <anthony@anthony-laptop.(none)>
2009-07-11 16:49:36 -0400

    adding page views

---
+++ source/foresmo/Foresmo/App/Admin/View/pages_new.php
@@ -0,0 +1,89 @@
+
+        <script type="text/javascript">
+        tinyMCE.init({
+            mode : "exact",
+            elements : "post_content",
+            width : "700",
+            height: "400",
+            verify_html : false,
+            apply_source_formatting : false,
+            fix_nesting : false,
+            fix_list_elements : false,
+            fix_content_duplication : false,
+            cleanup : false,
+            cleanup_on_startup: false,
+            trim_span_elements : false,
+            skin: 'thebigreason',
+            extended_valid_elements : "?php",
+            doctype : '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">',
+            theme : 'advanced',
+                plugins : 'safari,spellchecker,layer,save,advimage,advlink,inlinepopups,contextmenu,paste,noneditable,visualchars,nonbreaking,pagebreak',
+                theme_advanced_buttons1: 'bold,italic,underline,strikethrough,|,bullist,numlist,|,justifyleft,justifycenter,justifyright,|,link,unlink|,spellchecker,formatselect,|,image,charmap,|,outdent,indent,|,undo,redo,',
+                theme_advanced_buttons2: '',
+                theme_advanced_buttons3: '',
+                theme_advanced_toolbar_location : 'top',
+                theme_advanced_toolbar_align : 'left',
+                theme_advanced_statusbar_location : 'bottom',
+                theme_advanced_resize_horizontal : true,
+                theme_advanced_resizing : true,
+                apply_source_formatting : true,
+                theme_advanced_source_editor_width : '700',
+                spellchecker_languages : '+English=en,Danish=da,Dutch=nl,Finnish=fi,French=fr,German=de,Italian=it,Polish=pl,Portuguese=pt,Spanish=es,Swedish=sv'
+        });
+        </script>
+        <script>
+        window.addEvent('domready', function() {
+            $('post_new_submit').addEvent('click', function(e) {
+                e.stop();
+                fixTiny({instance:'post_content'});
+                $('post_new').set('send', {url: '/ajax', onComplete: function(response) {
+                    var response = JSON.decode(response);
+                    $('message').set('html', response.message);
+                    if (response.success) {
+                        window.location = '/admin/pages/edit/1';
+                    }
+                }});
+                $('post_new').send();
+            });
+        });
+        </script>
+        <div class="grid_16">
+            <h2 class="page-heading">Pages &#187; New</h2>
+            <h2 class="page-heading-right"><?php echo $this->blog_title;?> administration</h2>
+        </div>
+        <form action="/ajax" method="post" id="post_new" style="display:inline;">
+        <div class="grid_4">
+        <br/>
+
+            <div class="box">
+                <h2>Page Options</h2>
+                <div class="block">
+
+                    <label for="post_status">Status</label><br/>
+                    <select name="post_status">
+                        <option value="1">Publish</option>
+                        <option value="2">Save as draft</option>
+                    </select><br/><br/>
+                    <label for="post_tags">Tags</label><br/>
+                    <input type="text" name="post_tags" class="input" style="width: 90%;"/><br/>
+                    <span class="byline">Seperated by commas.</span>
+                    <br/><br/>
+                    <label for="post_comments_disabled">Disable Comments</label>
+                    <input type="checkbox" name="post_comments_disabled" value="true"/>
+                </div>
+            </div>
+        </div>
+        <div class="grid_7">
+            <label for="post_title">Page Title</label><br/>
+            <input type="text" name="post_title" class="input" style="width: 694px;"/><br/><br/>
+            <input type="hidden" name="ajax_action" value="admin_pages_new" />
+            <input type="hidden" name="post_type" value="2" />
+            <input type="textarea" id="post_content" name="post_content" /><br/>
+            <input type="submit" id="post_new_submit" name="submit" value="Submit" class="submit_input"/>
+
+            <br/>
+            <div id="message" class="message">
+            </div>
+        </div>
+        </form>
+        <div class="clear"></div>
---
+++ source/foresmo/Foresmo/App/Index/View/page.php
@@ -0,0 +1,75 @@
+
+    <h2><?php echo $this->posts['title'];?></h2>
+    <?php
+    if (!$this->comments_disabled) {
+        if (count($this->posts['comments']) == 1) {
+            echo count($this->posts['comments']) . ' ' . '<a href="/'.$this->posts['slug'].'#comments">comment</a>';
+        } else {
+            echo count($this->posts['comments']) . ' ' . '<a href="/'.$this->posts['slug'].'#comments">comments</a>';
+        }
+        echo '</span><br/><br/>';
+    }
+    ?>
+    <hr/>
+    <p><?php echo $this->posts['content'];?></p><br/>
+    <?php
+    $tags = array();
+    foreach ($this->posts['tags'] as $tag) {
+        $tags[] = "<span class=\"tag\"><a href=\"/tag/{$tag['tag_slug']}\" alt=\"{$tag['tag']}\">{$tag['tag']}</a></span>";
+    }
+    if (!empty($this->posts['tags'])) {
+        echo '<span>Tags: ' . implode(', ', $tags) . '</span>';
+    }
+    ?>
+
+<?php
+if (!$this->comments_disabled) {
+?>
+    <br/><br/>
+    <span><a name="comments">Comments</a> - <?php echo count($this->posts['comments']);?></span><br/><br/>
+<?php
+
+foreach($this->posts['comments'] as $key => $comment){
+
+    if ($comment['type'] == '1') {
+        echo "<div class=\"comment admin\">";
+    } else {
+        if ($key%2 == 1) {
+            echo "<div class=\"comment odd\">";
+        } else {
+            echo "<div class=\"comment even\">";
+        }
+    }
+    echo '<a name="comment-'.$comment['id'].'"></a>';
+    $default = 'http://' . $_SERVER['SERVER_NAME'] . "/public/Foresmo/{$this->blog_theme}/images/contact_grey.png";
+    $size = 50;
+    $grav_url = "http://www.gravatar.com/avatar.php?gravatar_id=".md5($comment['email'])."&default=".urlencode($default)."&size=".$size;
+
+    echo "<div class=\"comment-author\">";
+    if ($comment['url'] !== '') {
+        echo "<span style=\"font-size:0.8em;\"><a href=\"".$comment['url']."\" target=\"_blank\">".$comment['name']."</a></span><br />";
+    } else {
+        echo "<span style=\"font-size:0.8em;\">".$comment['name']."</span><br/>";
+    }
+
+    echo '<img src="'.$grav_url.'"/>';
+    echo "</div>";
+    echo "<div class=\"comment-message\">";
+    echo "<span style=\"font-size:0.6em;\">{$comment['date']}</span><br/>";
+    echo "<span style=\"font-size:0.9em;\">".$comment['content']."</span><br />";
+    echo "</div>";
+    echo "</div>";
+}
+?>
+<hr/>
+<h2>Post a Comment</h2>
+<br/>
+<?php
+echo $this->form;
+echo "\n<br/>";
+if (isset($this->msg)) {
+    echo $this->msg;
+}
+}
+?>
+
