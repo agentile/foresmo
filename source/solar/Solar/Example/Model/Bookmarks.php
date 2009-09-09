c5851fd604e32d9411a11630fadd3efd61747888
Anthony <anthony@anthony-laptop.(none)>
2009-09-08 21:25:11 -0500

    updating solar to revision 4000

---
+++ source/solar/Solar/Example/Model/Bookmarks.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * 
+ * Inherited model class.
+ * 
+ * @category Solar
+ * 
+ * @package Solar_Example
+ * 
+ * @author Paul M. Jones <pmjones@solarphp.com>
+ * 
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ * 
+ * @version $Id: Bookmarks.php 3995 2009-09-08 18:49:24Z pmjones $
+ * 
+ */
+class Solar_Example_Model_Bookmarks extends Solar_Example_Model_Nodes {
+    
+    /**
+     * 
+     * Model-specific setup.
+     * 
+     * @return void
+     * 
+     */
+    protected function _setup()
+    {
+        parent::_setup();
+    }
+}
